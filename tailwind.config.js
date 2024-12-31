74ac1342673fd9eed457172adb3ba6586f250039
Het Shah <shahhet6153@gmail.com>
2024-12-31 11:54:04 +0530

    font changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["DM Serif Text"],
+        serif: ["Open Sans"],
+        sans: ["PT Sans"],
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease-in-out",
+      },
+      keyframes: {
+        "fade-in": {
+          from: {
+            opacity: 0,
+          },
+          to: { opacity: 1 },
+        },
       },
     },
   },
